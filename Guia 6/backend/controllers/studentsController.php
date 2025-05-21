f46bc44f416c8989465b51e08273f452f82bdc87
CiroExe <mdqciro08@gmail.com>
2025-05-21 13:41:49 -0300

    Add files via upload

--- Guia 6/backend/controllers/studentsController.php
+++ Guia 6/backend/controllers/studentsController.php
@@ -1,25 +1,34 @@
 <?php
 require_once("./models/students.php");
 
-function handleGet($conn) {
-    if (isset($_GET['id'])) {
+function handleGet($conn) 
+{
+    if (isset($_GET['id'])) 
+    {
         $result = getStudentById($conn, $_GET['id']);
         echo json_encode($result->fetch_assoc());
-    } else {
+    } 
+    else 
+    {
         $result = getAllStudents($conn);
         $data = [];
-        while ($row = $result->fetch_assoc()) {
+        while ($row = $result->fetch_assoc()) 
+        {
             $data[] = $row;
         }
         echo json_encode($data);
     }
 }
 
-function handlePost($conn) {
+function handlePost($conn) 
+{
     $input = json_decode(file_get_contents("php://input"), true);
-    if (createStudent($conn, $input['fullname'], $input['email'], $input['age'])) {
+    if (createStudent($conn, $input['fullname'], $input['email'], $input['age'])) 
+    {
         echo json_encode(["message" => "Estudiante agregado correctamente"]);
-    } else {
+    } 
+    else 
+    {
         http_response_code(500);
         echo json_encode(["error" => "No se pudo agregar"]);
     }
@@ -27,19 +36,26 @@ function handlePost($conn) {
 
 function handlePut($conn) {
     $input = json_decode(file_get_contents("php://input"), true);
-    if (updateStudent($conn, $input['id'], $input['fullname'], $input['email'], $input['age'])) {
+    if (updateStudent($conn, $input['id'], $input['fullname'], $input['email'], $input['age'])) 
+    {
         echo json_encode(["message" => "Actualizado correctamente"]);
-    } else {
+    } 
+    else 
+    {
         http_response_code(500);
         echo json_encode(["error" => "No se pudo actualizar"]);
     }
 }
 
-function handleDelete($conn) {
+function handleDelete($conn) 
+{
     $input = json_decode(file_get_contents("php://input"), true);
-    if (deleteStudent($conn, $input['id'])) {
+    if (deleteStudent($conn, $input['id'])) 
+    {
         echo json_encode(["message" => "Eliminado correctamente"]);
-    } else {
+    } 
+    else 
+    {
         http_response_code(500);
         echo json_encode(["error" => "No se pudo eliminar"]);
     }
--- Guia 6/backend/controllers/subjectsController.php
+++ Guia 6/backend/controllers/subjectsController.php
@@ -1,45 +1,62 @@
 <?php
 require_once("./models/subjects.php");
 
-function handleGet($conn) {
-    if (isset($_GET['id'])) {
+function handleGet($conn) 
+{
+    if (isset($_GET['id'])) 
+    {
         $result = getSubjectById($conn, $_GET['id']);
         echo json_encode($result->fetch_assoc());
-    } else {
+    } 
+    else 
+    {
         $result = getAllSubjects($conn);
         $data = [];
-        while ($row = $result->fetch_assoc()) {
+        while ($row = $result->fetch_assoc()) 
+        {
             $data[] = $row;
         }
         echo json_encode($data);
     }
 }
 
-function handlePost($conn) {
+function handlePost($conn) 
+{
     $input = json_decode(file_get_contents("php://input"), true);
-    if (createSubject($conn, $input['subjectName'])) {
-        echo json_encode(["message" => "Materia agregado correctamente"]);
-    } else {
+    if (createSubject($conn, $input['name'])) 
+    {
+        echo json_encode(["message" => "Materia creada correctamente"]);
+    } 
+    else 
+    {
         http_response_code(500);
-        echo json_encode(["error" => "No se pudo agregar"]);
+        echo json_encode(["error" => "No se pudo crear"]);
     }
 }
 
-function handlePut($conn) {
+function handlePut($conn) 
+{
     $input = json_decode(file_get_contents("php://input"), true);
-    if (updateSubject($conn, $input['id'], $input['subjectName'])) {
-        echo json_encode(["message" => "Actualizado correctamente"]);
-    } else {
+    if (updateSubject($conn, $input['id'], $input['name'])) 
+    {
+        echo json_encode(["message" => "Materia actualizada correctamente"]);
+    } 
+    else 
+    {
         http_response_code(500);
         echo json_encode(["error" => "No se pudo actualizar"]);
     }
 }
 
-function handleDelete($conn) {
+function handleDelete($conn) 
+{
     $input = json_decode(file_get_contents("php://input"), true);
-    if (deleteSubject($conn, $input['id'])) {
-        echo json_encode(["message" => "Eliminado correctamente"]);
-    } else {
+    if (deleteSubject($conn, $input['id'])) 
+    {
+        echo json_encode(["message" => "Materia eliminada correctamente"]);
+    } 
+    else 
+    {
         http_response_code(500);
         echo json_encode(["error" => "No se pudo eliminar"]);
     }
