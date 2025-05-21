0968ff1cbf596ee60fc91bb91e4db5a83801bf76
CiroExe <mdqciro08@gmail.com>
2025-05-21 13:40:25 -0300

    Add files via upload

--- Guia 6/backend/routes/studentsRoutes.php
+++ Guia 6/backend/routes/studentsRoutes.php
@@ -1,23 +1,27 @@
 <?php
 require_once("./config/databaseConfig.php");
+require_once("./routes/routesFactory.php");
 require_once("./controllers/studentsController.php");
 
-switch ($_SERVER['REQUEST_METHOD']) {
-    case 'GET':
-        handleGet($conn);
-        break;
-    case 'POST':
+// routeRequest($conn);
+
+
+/**
+ * Ejemplo de como se extiende un archivo de rutas 
+ * para casos particulares
+ * o validaciones:
+ */
+routeRequest($conn, [
+    'POST' => function($conn) 
+    {
+        // Validación o lógica extendida
+        $input = json_decode(file_get_contents("php://input"), true);
+        if (empty($input['fullname'])) 
+        {
+            http_response_code(400);
+            echo json_encode(["error" => "Falta el nombre"]);
+            return;
+        }
         handlePost($conn);
-        break;
-    case 'PUT':
-        handlePut($conn);
-        break;
-    case 'DELETE':
-        handleDelete($conn);
-        break;
-    default:
-        http_response_code(405);
-        echo json_encode(["error" => "Método no permitido"]);
-        break;
-}
-?>
+    }
+]);
